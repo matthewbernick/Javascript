73dbcac99513ce7119f62fc1f8c2a6d542df04c9
matthewbernick <bernick.matthew@gmail.com>
2022-12-22 14:59:24 -0500

    Added javascript variable exercises

---
+++ variables.js
@@ -0,0 +1,54 @@
+// Intializing variables using var
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z)
+
+// Initializing variables using let
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z) 
+
+// Initializing undeclared variables
+x = 5;
+y = 6;
+z = x + y;
+console.log(z)
+
+// Initializing variables using const
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total);
+
+// Initializing variables using strings
+const pi = 3.14;
+let person = "John Doe";
+let answer = 'Yes I am!';
+console.log(pi,person,answer);
+
+// Declaring a variable then assigning a value
+let carName;
+console.log(carName);
+carName = "Mercedes";
+console.log(carName);
+
+// Adding and concatenating numbers and strings
+let x = 5 + 2 + 3;
+let y = "John" + " " + "Doe";
+let z = "5" + 2 + 3;
+console.log(x,y,z)
+
+// Using $ as an identifier
+let $ = "Hello World";
+let $$$ = 2;
+let $myMoney = 5;
+console.log($,$$$);
+console.log($myMoney)
+
+// Using _ as an identifier
+let _lastName = "Johnson";
+let _x = 2;
+let _100 = 5;
+console.log(_x)
